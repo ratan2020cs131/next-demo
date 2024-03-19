d33e1d2df57423e09c1469a6b642d68243876816
Greko <gyan2020cs093@abesit.edu.in>
2024-03-19 23:39:50 +0530

    Dine

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,19 @@
 import Navbar from '@/components/Navbar'
 import Upload from '@/components/upload';
-
+import Table from '@/components/Table'
+import {Divider} from "@nextui-org/react";
+import {NextUIProvider} from "@nextui-org/react";
 export default function Home() {
   return (
-    <main>
+    
+    <main className="dark text-foreground bg-background" >
       <Navbar />
+      <Divider className="my-4" />
+    
       <Upload />
+      
+      <Divider className="my-4" />
+      <Table />
     </main>
   );
 }
---
+++ src/components/uploadBox.jsx
@@ -0,0 +1,13 @@
+'use client'
+import React, { useState, useRef } from 'react'
+import Image from 'next/image'
+import styles from './style.module.css';
+
+
+const UploadBox = ()=>{
+return(
+    <main>
+    
+    </main>
+);
+}
