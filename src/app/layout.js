4eddf52a856bf427d309e165a1485eb003ab2241
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-03-19 13:25:13 +0530

    bug fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,12 +10,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <NextUI>
+    // <NextUI>
       <html lang="en">
         <body className={inter.className}>
           {children}
         </body>
       </html>
-    </NextUI>
+    // </NextUI>
   );
 }
